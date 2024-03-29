03cdce7c59e6dd42034eafedff107f91c9127711
Devinthedevleoper <dxtofocused@gmail.com>
2024-03-28 22:59:59 -0500

    added an asset folder

---
+++ Asset/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const usernameInput = document.getElementById('User');
+    const titleInput = document.getElementById('Title');
+    const contentInput = document.getElementById('Content');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const Uservalue = usernameInput.value;
+        const Titlevalue = titleInput.value;
+        const Contentvalue = contentInput.value;
+
+        if (!Uservalue || !Titlevalue || !Contentvalue) {
+            alert('Please complete all fields in the form.');
+            return; // Stop further execution if any field is empty
+        }
+
+        console.log('Username:', Uservalue);
+        console.log('Title:', Titlevalue);
+        console.log('Content:', Contentvalue);
+
+        localStorage.setItem('User', Uservalue);
+        localStorage.setItem('Title', Titlevalue);
+        localStorage.setItem('Content', Contentvalue);
+
+        usernameInput.value = '';
+        titleInput.value = '';
+        contentInput.value = '';
+
+        alert('Data submitted and stored in local storage!');
+
+        // Redirect to the posts page
+        window.location.href = 'blog.html'; // Replace 'blog.html' with the actual URL of your posts page
+    });
+
+    document.getElementById('backButton').addEventListener('click', function(event) {
+        event.preventDefault();
+        window.history.back();
+    });
+
+});
